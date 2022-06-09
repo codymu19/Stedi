702ebb0d5844e27ced57fb8be778c435fc6abea3
Cody Murdock <y4cody@gmail.com>
2022-06-09 13:46:01 -0600

    App good

--- login.js
+++ login.js
@@ -6,7 +6,7 @@ const Login = () => {
   const [OneTimePassword, setOneTimePassword] = useState(null);
 
   return (
-    <SafeAreaView>
+    <SafeAreaView style={styles.mainView}>
       <TextInput
         style={styles.input}
         onChangeText={setPhoneNumber}
@@ -32,6 +32,9 @@ const styles = StyleSheet.create({
     borderWidth: 1,
     padding: 10,
   },
+  mainView:{
+    marginTop:250
+  }
 });
 
 export default Login;
