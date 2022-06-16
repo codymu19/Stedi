bd1c23dc4854b05e18bf07c5bdbadb62aec31d20
Cody Murdock <y4cody@gmail.com>
2022-06-16 13:46:08 -0600

    sent text button may work

--- login.js
+++ login.js
@@ -1,11 +1,23 @@
 import {useState} from "react";
 import { SafeAreaView, StyleSheet, TextInput, Text, TouchableOpacity, View } from "react-native";
 
+const sendText = async (phoneNumber) => {
+  // using fetch do a Post to http://dev.stedi.me/twofactorlogin/###-###-####
+  await fetch('http://dev.stedi.me/twofactorlogin/' + phoneNumber, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/text'
+    },
+  })
+  console.log("Phone Number:",phoneNumber);
+
+};
+
 const Login = () => {
   const [phoneNumber, setPhoneNumber] = useState("");
   const [OneTimePassword, setOneTimePassword] = useState(null);
-  const [count, setCount] = useState(0);
-  const onPress = () => setCount(prevCount => prevCount + 1);
+  // const [count, setCount] = useState(0);
+  // const onPress = () => setCount(prevCount => prevCount + 1);?
 
   return (
     <SafeAreaView style={styles.mainView}>
@@ -25,7 +37,17 @@ const Login = () => {
       />
         <TouchableOpacity
           style={buttonStyle.button}
-          onPress={onPress}
+          // onPress={onPress}
+          // onPress={()=>{console.log('Login button was clicked')}}
+          onPress={()=>{sendText(phoneNumber)}}
+        >
+          <Text style={buttonStyle.text}>Send Text</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={buttonStyle.button}
+          // onPress={onPress}
+          // onPress={()=>{console.log('Login button was clicked')}}
+          onPress={()=>{sendText(phoneNumber)}}
         >
           <Text style={buttonStyle.text}>Login</Text>
         </TouchableOpacity>
@@ -41,7 +63,9 @@ const styles = StyleSheet.create({
     padding: 10,
   },
   mainView:{
-    margin: "auto",
+    margin: 30,
+    marginTop: 250,
+    // justifyContent: "center",
     justifyItems: "center",
   }
 });
@@ -55,19 +79,19 @@ const buttonStyle = StyleSheet.create({
   },
   button: {
     alignItems: "center",
-    backgroundColor: "#DDDDDD",
-    padding: 10
+    // backgroundColor: "#DDDDDD",
+    backgroundColor: "#6BDB4F",
+    padding: 10,
+    margin: 5,
+    marginRight: 70,
+    marginLeft: 70
   },
-  countContainer: {
-    alignItems: "center",
-    padding: 10
+  text: {
+    color: "#FFFFFF",
+    fontWeight: "800",
+    fontSize: 30,
+
   }
-  // text: {
-  //   zIndex: 5,
-  //   // backgroundColor: "#000000",
-  //   fontSize: 20,
-  //   padding: 50
-  // }
 });
 
 
