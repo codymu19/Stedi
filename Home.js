4369e16b4f1463af8795a72bbdcd10824d318d81
Cody Murdock <y4cody@gmail.com>
2022-07-11 12:03:50 -0600

    added props

--- Home.js
+++ Home.js
@@ -5,28 +5,10 @@ import Icons from './Icons.js';
 import Bar  from './Bar.js';
 import {useState} from "react";
 
-// const validateToken = () => {
-//   let tokenEmail="";
-//   $.ajax({
-//      type: 'GET',
-//       url: 'https://dev.stedi.me/validate/'+usertoken,
-//       success: function(data){
-//          if (data==""){
-//            window.location="/"
-//          } else{
-//            $('#email').html(data);
-//          }
-//       },//token is no longer valid (1 hour expiration), they need to log in
-//       contentType: "application/text",
-//       dataType: 'text' })
 
-//   return tokenEmail;
-// }
-
-
-const Home = () => {
+const Home = (props) => {
   const [userName, setUserName] = useState(props.userName);
-  console.log(props.userName)
+  // console.log(props.userName)
   return (
     <View>
       <Bar userName= {props.userName} />
